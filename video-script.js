5731986654b0d13c2167762c04016d27daefe7b3
M Strom <157034943+mstromberg3@users.noreply.github.com>
2025-04-18 19:27:33 -0400

    update video-script.js

---
+++ video-script.js
@@ -0,0 +1,26 @@
+const affirmationContainer = document.getElementById("video-affirmation");
+const btn = document.getElementById("btn");
+
+// Create a list of the videos
+const videos = [];
+
+// loop to add each video to array
+let i = 0;
+
+while (i < 4) {
+    videos[i] = ("images/Layer" + (i + 2) + ".png");
+    i++;
+}
+
+// get a random number and return that in array
+let getAffirmation = () => {
+    affirmationContainer.classList.remove("fade-out");
+    const randomNumber = Math.floor(Math.random() * 4);
+    const newVideo = videos[randomNumber];
+    affirmationContainer.innerHTML = "<img src='" + newVideo + "' class='affirmation'/>";
+    affirmationContainer.classList.add("fade-in");
+    console.log(randomNumber);
+}
+
+btn.addEventListener("click", getAffirmation);
+getAffirmation();
